58e5fa8f2fc9128bd1ea70dd822dbb11b6e5b51a
ccci210 <cindy1024iii@gmail.com>
2025-05-02 02:16:52 -0700

    add quill 5.2

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,8 +1,35 @@
+import { useUser } from "@clerk/clerk-react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css"; // import styles
+
 const Write = () => {
+  const { isLoaded, isSignedIn } = useUser();
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  if (!isLoaded && !isSignedIn) {
+    return <div className="">You should login!</div>;
+  }
+
   return (
-    <div>
-      <h1>Write</h1>
-      <p>This is the Write page.</p>
+    <div className="flex flex-col">
+      <h1>Create a new post</h1>
+      <form className="flex flex-col">
+        <button>add a cover image</button>
+        <input type="text" placeholder="Title" />
+        <div className="flex">
+          <label htmlFor="content">CHoose </label>
+          <select name="cat" id="">
+            <option value="text">General</option>
+            <option value="image">Web design</option>
+            <option value="video">development</option>
+            <option value="audio">database</option>
+          </select>
+        </div>
+        <textarea name="desc" placeholder="Description" />
+        <ReactQuill theme="snow" />
+        <button>Send</button>
+      </form>
     </div>
   );
 };
