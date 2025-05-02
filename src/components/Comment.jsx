8f77367652c6639b5cbe195f61199cda0e9637ae
ccci210 <cindy1024iii@gmail.com>
2025-05-01 23:07:57 -0700

    add Comment component 5.1

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,27 @@
-const Comment = ({ comment }) => {
+import Image from "./Image";
+
+const Comment = () => {
   return (
-    <div className="comment">
-      <h4>{comment.name}</h4>
-      <p>{comment.body}</p>
+    <div className="p-4 bg-slate-50 rounded-xl mb-8">
+      <div className="flex items-center gap-4">
+        <Image
+          src="userImg.jpeg"
+          className="w-10 h-10 rounded-full object-cover"
+          width="40"
+        />
+        <span className="font-medium">John doe</span>
+        <span className="text-gray-500 text-sm">2 days ago</span>
+      </div>
+      <div className="mt-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+          reprehenderit, doloremque, voluptatibus cumque, eveniet quidem
+          quibusdam minima iusto deserunt magnam asperiores alias. Repudiandae
+          reiciendis, voluptatibus quisquam velit autem cumque maiores.
+        </p>
+      </div>
     </div>
   );
 };
+
 export default Comment;
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,25 @@
+import Comment from "./Comment";
+
 const Comments = () => {
   return (
-    <div className="comments">
-      <h2>Comments</h2>
-      <p>This is the comments section.</p>
+    <div className="flex flex-col gap-8 lg:w-3/5">
+      <h2 className="text-xl text-gray-500 underline">Comments</h2>
+      <div className="flex items-center justify-between gap-8 w-full">
+        <textarea
+          placeholder="Write a comment"
+          className="w-full p-4 rounded-xl"
+        />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          Send
+        </button>
+      </div>
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
     </div>
   );
 };
