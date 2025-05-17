0bc793f9335209acbf6f0ffcf15e96bc1df561c5
ccci210 <cindy1024iii@gmail.com>
2025-05-17 02:19:07 -0700

    Update controller 5.17

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -9,11 +9,35 @@ export const getPost = async (req, res) => {
   res.status(200).send(post);
 };
 export const createPost = async (req, res) => {
-  const newPost = await Post.create(req.body);
+  const clearUserId = req.auth.userId;
+  if (!clearUserId) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  const user = await User.findOne({ clerkUserId: clearUserId });
+  if (!user) {
+    return res.status(401).json({
+      message: "User not found",
+    });
+  }
+
+  const newPost = await Post.create({ user: user._id, ...req.body });
   const post = await newPost.save();
   res.status(200).json(post);
 };
 export const deletePost = async (req, res) => {
-  const post = await Post.findByIdAndDelete(req.params.id);
+  const clearUserId = req.auth.userId;
+  if (!clearUserId) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  const user = await User.findOne({ clerkUserId: clearUserId });
+  const post = await Post.findByIdAndDelete({
+    user: user._id,
+    _id: req.params.id,
+  });
   res.status(200).json(post);
 };
--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,29 @@ import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import { connectDB } from "./lib/connectDB.js";
 import webhookRouter from "./routes/webhook.route.js";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 const app = express();
+app.use(clerkMiddleware());
 app.use("/webhooks", webhookRouter);
 app.use(express.json());
+
+// app.get("/auth-state", (req, res) => {
+//   const authState = req.auth;
+//   res.json(authState);
+// });
+
+app.get("/protect", (req, res) => {
+  const { userId } = req.auth;
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  res.status(200).json("content");
+});
+
+// app.get("/protect2", requireAuth(), (req, res) => {
+//   res.status(200).json("content");
+// });
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
