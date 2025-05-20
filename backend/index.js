9795ae52634ef0a3ea33fae6d73dd7847b31873d
Jingdi Zhang <jingdizhang@esri.com>
2025-05-20 14:32:29 -0700

    test push 5.20

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.use(cors(process.env.CLIENT_URL));
 app.use(clerkMiddleware());
 app.use("/webhooks", webhookRouter);
 app.use(express.json());
+console.log("test")
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
