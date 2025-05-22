38a0bd3c1b5b278e70b496e765454e896617f78c
ccci210 <cindy1024iii@gmail.com>
2025-05-21 22:42:04 -0700

    add infinite scroll 5.21

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -2,6 +2,7 @@ import PostListItem from "./PostListItem";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 // const fetchPosts = async (pageParam) => {
 //   const response = await axios.get(
@@ -16,7 +17,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 const fetchPosts = async (pageParam) => {
   console.log("Page param", pageParam); // Make sure it's a number or valid object
   const response = await axios.get(`${import.meta.env.VITE_API_URL}/posts`, {
-    params: { page: pageParam },
+    params: { page: pageParam, limit: 2 },
   });
   return response.data;
 };
@@ -44,11 +45,27 @@ const PostList = () => {
   const allPosts = data?.pages?.flatMap((page) => page.posts) || [];
   console.log(data, allPosts);
   return (
-    <div className="flex flex-col gap-12 mb-8">
+    // <div className="flex flex-col gap-12 mb-8">
+    //   {allPosts.map((post) => (
+    //     <PostListItem key={post._id} post={post} />
+    //   ))}
+    // </div>
+    <InfiniteScroll
+      dataLength={allPosts.length} //This is important field to render the next data
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<h4>Loading more posts...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>All posts loaded!</b>
+        </p>
+      }
+      // below props only if you need pull down functionality
+    >
       {allPosts.map((post) => (
         <PostListItem key={post._id} post={post} />
       ))}
-    </div>
+    </InfiniteScroll>
   );
 };
 
