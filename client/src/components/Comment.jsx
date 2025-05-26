7b2ae51b1f7bccf0571a31575f5a1dba26a65a2e
ccci210 <cindy1024iii@gmail.com>
2025-05-25 19:37:03 -0700

    load comments in a page 5.25

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,24 +1,26 @@
 import Image from "./Image";
+import { format } from "timeago.js";
 
-const Comment = () => {
+const Comment = ({ comment }) => {
   return (
     <div className="p-4 bg-slate-50 rounded-xl mb-8">
       <div className="flex items-center gap-4">
-        <Image
-          src="userImg.jpeg"
-          className="w-10 h-10 rounded-full object-cover"
-          width="40"
-        />
-        <span className="font-medium">John doe</span>
-        <span className="text-gray-500 text-sm">2 days ago</span>
+        {comment?.user.img && (
+          <Image
+            src="userImg.jpeg"
+            className="w-10 h-10 rounded-full object-cover"
+            width="40"
+          />
+        )}
+        <span className="font-medium">
+          {comment?.user?.username ?? "default"}
+        </span>
+        <span className="text-gray-500 text-sm">
+          {format(comment.createdAt)}
+        </span>
       </div>
       <div className="mt-4">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-          reprehenderit, doloremque, voluptatibus cumque, eveniet quidem
-          quibusdam minima iusto deserunt magnam asperiores alias. Repudiandae
-          reiciendis, voluptatibus quisquam velit autem cumque maiores.
-        </p>
+        <p>{comment.desc}</p>
       </div>
     </div>
   );
